3954caf872a8d3418d95f2f50cec7782ec4046c0
Scott Pledger <scottpledger2005@gmail.com>
2014-05-14 08:52:05 -0600

    push

--- docs/assets/behavior.js
+++ docs/assets/behavior.js
@@ -31,6 +31,14 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
               "type": "heading",
               "data": {
                 "level": 2,
+                "title": "Quick Links",
+                "slug": "quick-links"
+              },
+              "depth": 2
+            }, {
+              "type": "heading",
+              "data": {
+                "level": 2,
                 "title": "Getting Set Up",
                 "slug": "getting-set-up"
               },
@@ -67,6 +75,14 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
       "depth": 1,
       "outline": [
         {
+          "type": "heading",
+          "data": {
+            "level": 2,
+            "title": "Quick Links",
+            "slug": "quick-links"
+          },
+          "depth": 2
+        }, {
           "type": "heading",
           "data": {
             "level": 2,
