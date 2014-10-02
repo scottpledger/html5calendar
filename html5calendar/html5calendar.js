938669abbff9ea6790db151381d3ba69ccc0bd16
Scott Pledger <scottpledger2005@gmail.com>
2014-10-02 13:45:09 -0600

    push

--- html5calendar/html5calendar.js
+++ html5calendar/html5calendar.js
@@ -50,8 +50,8 @@ HT5Calendar = (function(undefined) {
 		this.viewContainer.append(this.weeksList);
 		this.eventData=this.element.find('[data-calendar-events]');
 		this.today.startOf('day');
-		this._initButtons();
-		this._parseEvents();
+		private._initButtons(this);
+		private._parseEvents(this);
 		this.innerElement.removeClass('agenda basic month')
 			.addClass(this.options.type);
 		this.element.removeClass('ht5c-agenda ht5c-basic ht5c-month')
@@ -59,73 +59,301 @@ HT5Calendar = (function(undefined) {
 		
 		this.render();
 
-		this._mouseInit();
+		private._mouseInit(this);
 	}
 
-	HT5Calendar.prototype._setOption = function(name, value) {
+	var public = {
+		render: function() {
+			private._layoutHourRows(this);
+			private._updateView(this);
+		},
+		next: function(duration) {
+			var args = Array.prototype.slice.apply(arguments,[0]),
+				defDur = this.options.view=='month' ? 'months': 'weeks';
 
-	}
-
-	HT5Calendar.prototype._initButtons = function() {
-		var self = this;
-		this.element.find('[data-calendar-action]').on('click',function() {
-			var $t=$(this);
-			self.element.html5calendar($t.attr('data-calendar-action'));
-		});
-		this.element.find('[data-calendar-option]').on('click',function() {
-			var $t=$(this);
-			self.element.html5calendar('option',$t.attr('data-calendar-option'),$t.attr('data-calendar-optionValue'));
-		});
-	}
-	
-	HT5Calendar.prototype._parseEvents = function() {
-		var self=this,
-			hourBegin = moment.duration(this.options.dayBegin),
-			hourEnd = moment.duration(this.options.dayEnd),
-			maxHeight = hourEnd.subtract(hourBegin).asHours();
-		this._hourBegin = hourBegin;
-		this._hourEnd = hourEnd;
-		this._maxHeight = maxHeight;
-		this.innerElement
-			.removeClass('days-1 days-2 days-3 days-4 days-5 days-6 days-7')
-			.addClass('days-'+this.options.daysPerWeek);
-		this.eventData.children('li').each(function(i,el) {
-			var ev = new HT5CEvent(el);
-			self._placeEvent(ev);
-		});
-	};
-	
-	HT5Calendar.prototype._placeEvent= function(ev) {
-		var $segs = ev.makeSegments('days'), self=this;
-
-		$segs.each(function(i,el) {
-			var $el = $(el), dims,top,height,cssData;
-			$el.appendTo(self._getDay($el.data('range').start));
-			dims = $el.data('dimensions');
-			top = dims.top-self._hourBegin.asHours();
-			height = dims.height;
-			if(top<0) {
-				top =0;
-				height += top;
+			if(args.length===0) {
+				this.defaultDate.add(1, defDur);
+			} else {
+				this.defaultDate.add.apply(this.defaultDate,args);
 			}
-			height = Math.min(self._maxHeight,height);
-			cssData = {
-				top: top+'em',
-				height: height+'em'
-			};
-			$el.data('css',cssData);
-			$el.css(self.options.view=='agenda' ? cssData: {
-				top: '0em',
-				height: 'auto'
+			private._updateView(this);
+		},
+		prev: function(duration) {
+			var args = Array.prototype.slice.apply(arguments,[0]),
+				defDur = this.options.view=='month' ? 'months': 'weeks';
+			if(args.length===0) {
+				this.defaultDate.subtract(1, defDur);
+			} else {
+				this.defaultDate.subtract.apply(this.defaultDate,args);
+			}
+			private._updateView(this);
+		}
+	};
+	
+	var private = {
+		_initButtons: function(t) {
+			var self = t;
+			this.element.find('[data-calendar-action]').on('click',function() {
+				var $t=$(this);
+				self.element.html5calendar($t.attr('data-calendar-action'));
 			});
-		});
+			this.element.find('[data-calendar-option]').on('click',function() {
+				var $t=$(this);
+				self.element.html5calendar('option',$t.attr('data-calendar-option'),$t.attr('data-calendar-optionValue'));
+			});
+		},
+		_parseEvents: function(t) {
+			var self=t,
+				hourBegin = moment.duration(t.options.dayBegin),
+				hourEnd = moment.duration(t.options.dayEnd),
+				maxHeight = hourEnd.subtract(hourBegin).asHours();
+			t._hourBegin = hourBegin;
+			t._hourEnd = hourEnd;
+			t._maxHeight = maxHeight;
+			t.innerElement
+				.removeClass('days-1 days-2 days-3 days-4 days-5 days-6 days-7')
+				.addClass('days-'+t.options.daysPerWeek);
+			t.eventData.children('li').each(function(i,el) {
+				var ev = new HT5CEvent(el);
+				self._placeEvent(ev);
+			});
+		},
+		_placeEvent: function(t,ev) {
+			var $segs = ev.makeSegments('days'), self=t;
 
-		ev.redraw();
+			$segs.each(function(i,el) {
+				var $el = $(el), dims,top,height,cssData;
+				$el.appendTo(self._getDay($el.data('range').start));
+				dims = $el.data('dimensions');
+				top = dims.top-self._hourBegin.asHours();
+				height = dims.height;
+				if(top<0) {
+					top =0;
+					height += top;
+				}
+				height = Math.min(self._maxHeight,height);
+				cssData = {
+					top: top+'em',
+					height: height+'em'
+				};
+				$el.data('css',cssData);
+				$el.css(self.options.view=='agenda' ? cssData: {
+					top: '0em',
+					height: 'auto'
+				});
+			});
+
+			ev.redraw();
+		},
+		_getWeek: function(t,mDay) {
+
+			mDay=moment(mDay).startOf('week');
+			var weekLabel = mDay.format("YYYYww");
+
+			if(t.weeks[weekLabel]!==undefined) {
+				return t.weeks[weekLabel];
+			}
+
+			var	week = $('<ul data-calendar-weekLabel="'+weekLabel+'" class="ht5c-days"></ul>'),
+				weekLi = $('<li class="ht5c-week"></li>'), k;
+
+			t.weeks[weekLabel]=week;
+
+			week.data('date',mDay);
+			weekLi.data('diff',mDay.diff(moment(t.today).startOf('week'),'weeks',true));
+
+			weekLi.append(week);
+
+			private._populateWeek(t,week,mDay);
+
+			for(k in t.weeks) {
+				if(k>weekLabel) {
+					t.weeks[k].parent().before(weekLi);
+					return week;
+				}
+			}
+
+			t.weeksList.append(weekLi);
+
+			return week;
+		},
+		_populateWeek: function(t,week,mD) {
+			var mDay = moment(mD),k;
+
+			for(k=0; k < t.options.daysPerWeek; k++) {
+				week.append(private._makeDay(t,mDay));
+				mDay.add(1,'days');
+			}
+		},
+		_makeDay: function(t,mD) {
+			var mDay = moment(mD),
+				fullDayLabel = mDay.format('YYYYDDDD'),
+					dayLabel = dayLabel = mDay.format(t.options.dayLabelFormat),
+					day = $('<ul data-calendar-dayLabel="'+dayLabel+'" class="ht5c-events"></ul>'),
+					dayLi = $('<li class="ht5c-day year-'+mDay.format('YYYY')+' month-'+mDay.format('M')+' day-'+mDay.format('D')+'"><time>'+dayLabel+'</time></li>');
+			dayLi.append(day);
+			t.days[fullDayLabel] = day;
+			return dayLi;
+		},
+		_getDay: function(t,mD) {
+			var mDay=moment(mD).startOf('day');
+			var fullDayLabel=mDay.format('YYYYDDDD'),
+				week=private._getWeek(t,mDay);
+
+			if(t.days[fullDayLabel]!==undefined) {
+				return t.days[fullDayLabel];
+			}
+
+			var	dayLi = private._makeDay(t,mDay);
+			week.append(dayLi);
+
+			for(k in week.days) {
+				if(k>dayLabel) {
+					week.days[k].parent().before(dayLi);
+				}
+			}
+
+			return day;
+		},
+		_layoutHourRows: function(t) {
+			var t0 = moment(t.today).startOf('day').add(moment.duration(t.options.dayBegin)),
+				dt = moment.duration(t.options.slotDuration),
+				tM = moment(t0).startOf('day').add(moment.duration(t.options.dayEnd)),
+				prev='',
+				p=dt.asHours(),
+				n=0;
+			t.hourSize = p;
+
+			while(t0.isBefore(tM) && n<100) {
+				var $li=$('<li class="ht5c-hour"></li>'),
+						label='<time>'+t0.format(t.options.rowLabelFormat)+'</time>';
+				if(label!=prev) {
+					$li.append(label);
+				}
+				prev=label;
+				t.hourRows.append($li);
+				$li.css({
+					top: n*p+'em',
+					height: p+'em',
+					'line-height': p+'em'
+				});
+
+				t.hourHeight = $li.height();
+
+				t0.add(dt);
+				n++;
+			}
+
+			t.innerElement.data('hours-height',(0.5+n*p));
+		},
+		_layoutDays: function(t) {
+			var self=t,
+				tBegin = moment(t.defaultDate);
+
+			var t0 = moment(tBegin),
+				dt = moment.duration(1,"day"),
+				n=0,$el;
+
+			for(n=0;n<t.options.daysPerWeek;n++) {
+				$el = private._getDay(t,t0);
+				t.dayHeight = $el.parent('li').height();
+				t0=moment(t0).add(1,"days");
+			}
+		},
+		_layoutWeeks: function(t) {
+			t.defaultDate.startOf('week').add(t.options.firstDay,'day');
+			private._layoutDays(t);
+			t.innerElement.css('height',t.innerElement.data('hours-height')+'em');
+
+		},
+		_layoutMonths: function(t) {
+			var mOrig = moment(t.defaultDate),
+				mLast = moment(t.defaultDate).endOf('month'),n=1;
+			t.defaultDate.startOf('month');
+
+			while(t.defaultDate.isBefore(mLast)) {
+				private._layoutWeeks(t);
+				t.defaultDate.add(1,'week');
+				n++;
+			}
+
+			t.defaultDate = mOrig;
+			t.innerElement.css('height',(n-1)+'em');
+		},
+		_updateWeeks: function(t) {
+			var weekLi;
+			for(var k in t.weeks) {
+				weekLi=t.weeks[k].parent();
+
+				weekLi.css(t.options.view=='agenda' ? {
+					left: (weekLi.data('diff')*100)+"%",
+					top: 0
+				}: {
+					left: 0,
+					top: (weekLi.data('diff'))+'em'
+				});
+			}
+		},
+		_updateEvents: function(t) {
+			var self=t;
+			t.innerElement.find('li.ht5c-event').each(function(i,el) {
+				var $el=$(el);
+
+				$el.css(self.options.view=='agenda' ? $el.data('css'): {
+					top: '0em',height: 'auto'
+				})
+			});
+		},
+		_updateView: function(t) {
+			var tBegin = moment(t.defaultDate);
+			private._updateEvents(t);
+
+			if(t.options.view=='agenda') {
+				t.element.find('[data-calender-receive="defaultDate"]')
+				.html(moment.range(tBegin.startOf('week'),moment(tBegin).add(t.options.daysPerWeek-1,'days'))
+					.format({
+						__default__: ['MMM D','MMM D YYYY']
+					}));
+
+				private._layoutWeeks(t);
+				private._updateWeeks(t);
+				t.weeksList.css({
+					top: 0,
+					left: (moment(t.today).startOf('week').diff(moment(t.defaultDate).startOf('week'), 'days')*t.colWidth)+"%"
+				});
+			} else if(t.options.view=='basic') {
+				t.element.find('[data-calender-receive="defaultDate"]')
+				.html(moment.range(tBegin,moment(tBegin).add(t.options.daysPerWeek,'days'))
+					.format({
+						__default__: ['MMM D','MMM D YYYY']
+					}));
+
+				private._layoutWeeks(t);
+				private._updateWeeks(t);
+				t.weeksList.css({
+					top: 0,
+					left: (moment(t.today).startOf('week').diff(moment(t.defaultDate).startOf('week'), 'days')*t.colWidth)+"%"
+				});
+			} else if(t.options.view=='month') {
+				t.element.find('[data-calender-receive="defaultDate"]')
+				.html(tBegin.format(t.options.monthLabel));
+				t.element.removeClass('month-1 month-2 month-3 month-4 month-5 month-6 month-7 month-8 month-9 month-10 month-11 month-12')
+					.addClass('month-'+t.defaultDate.format('M'));
+				private._layoutMonths(t);
+				private._updateWeeks(t);
+				t.weeksList.css({
+					left: 0,
+					top: (moment(t.today).startOf('week').diff(moment(t.defaultDate).startOf('month').startOf('week'), 'weeks'))+"em"
+				});
+			}
+		}
 	};
-
-	HT5Calendar.prototype.render = function() {
-
-	};
+	
+	
+	
+	
+	$.extend(HT5Calendar.prototype,public);
+	
 
 	HT5Calendar._getOptsFromElement = function($el) {
 		var val,opts = {};
